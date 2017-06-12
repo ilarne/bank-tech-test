6b57ece470e1333f1634bdc166a294bf5d979658
isabellarner <larner.isabel@gmail.com>
2017-06-12 16:51:15 +0100

    change printer test to reflect acceptance criteria

--- spec/PrinterSpec.js
+++ spec/PrinterSpec.js
@@ -4,10 +4,12 @@ describe("Printer", function() {
   var printer = new Printer;
 
   it('prints transaction history', function() {
-    account.depositAmount(1000, '15/01/2012')
-    account.depositAmount(1000, '16/01/2012')
+    account.depositAmount(1000, '10/01/2012')
+    account.depositAmount(2000, '13/01/2012')
+    account.withdrawAmount(500, '14/01/2012')
     expect(printer.printStatement(account)).toEqual("date || credit || debit || balance"+'\n'+
-    "16/01/2012 || 1000 || 0 || 2000"+'\n'+
-    "15/01/2012 || 1000 || 0 || 1000"+'\n');
+    "14/01/2012 || 0 || 500 || 2500"+'\n'+
+    "13/01/2012 || 2000 || 0 || 3000"+'\n'+
+    "10/01/2012 || 1000 || 0 || 1000"+'\n');
   });
 });
